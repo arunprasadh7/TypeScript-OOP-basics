47dc47d80130969f42a1a09f73b20499af2958a3
arunprasadh7 <m.arunprasadh96@gmail.com>
2024-06-12 20:53:04 +0530

    1 Declaring variables

---
+++ hello/main.js
@@ -0,0 +1,12 @@
+var message = "Hello World!";
+function greet(message) {
+    console.log(message);
+}
+greet(message);
+function doSomething() {
+    for (var i = 0; i <= 5; i++) {
+        console.log(i);
+    }
+    console.log("Finally ".concat(i));
+}
+doSomething();
---
+++ hello/main.ts
@@ -0,0 +1,16 @@
+let message = "Hello World!";
+
+function greet(message) {
+  console.log(message);
+}
+
+greet(message);
+
+function doSomething() {
+  for (let i = 0; i <= 5; i++) {
+    console.log(i);
+  }
+  console.log(`Finally ${i}`);
+}
+
+doSomething();
