3daf1e7ccb8de728e6fb828f81565a38953fda74
arunprasadh7 <m.arunprasadh96@gmail.com>
2024-06-12 21:05:48 +0530

    2 TypeScript types

---
+++ hello/types.js
@@ -0,0 +1,18 @@
+// TypeScript Types
+var a;
+var b;
+var c;
+var d;
+var e = [1, 2, 3];
+var f = [1, true, "hello"];
+var colorRed = 0;
+var colorGreen = 1;
+var colorBlue = 2;
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+    Color[Color["Purple"] = 3] = "Purple";
+})(Color || (Color = {}));
+var backgroundColor = Color.Blue;
---
+++ hello/types.ts
@@ -0,0 +1,20 @@
+// TypeScript Types
+
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[] = [1, 2, 3];
+let f: any[] = [1, true, "hello"];
+
+const colorRed = 0;
+const colorGreen = 1;
+const colorBlue = 2;
+
+enum Color {
+  Red = 0,
+  Green = 1,
+  Blue = 2,
+  Purple = 3,
+}
+let backgroundColor = Color.Blue;
