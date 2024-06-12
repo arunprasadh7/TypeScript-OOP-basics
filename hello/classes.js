7e09a0930fe6182d2467072214890d3462b59812
arunprasadh7 <m.arunprasadh96@gmail.com>
2024-06-13 00:37:42 +0530

    6 Classes

---
+++ hello/classes.js
@@ -0,0 +1,12 @@
+// Classes
+var Point = /** @class */ (function () {
+    function Point() {
+    }
+    Point.prototype.draw = function () {
+        //..
+    };
+    Point.prototype.getDistance = function (anotherPoint) {
+        //..
+    };
+    return Point;
+}());
---
+++ hello/classes.ts
@@ -0,0 +1,14 @@
+// Classes
+
+class Point {
+  x: number;
+  y: number;
+
+  draw() {
+    //..
+  }
+
+  getDistance(anotherPoint: Point) {
+    //..
+  }
+}
