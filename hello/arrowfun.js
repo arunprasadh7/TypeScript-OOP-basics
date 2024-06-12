34a960710c04a56a3c7e0ede82cc4be239e96d65
arunprasadh7 <m.arunprasadh96@gmail.com>
2024-06-13 00:20:25 +0530

    4 Arrow functions

---
+++ hello/arrowfun.js
@@ -0,0 +1,9 @@
+// Arrow functions
+// normal functoion
+function greet(name) {
+    console.log("Hello ".concat(name));
+}
+greet("Arun");
+// Arrow function
+var greet1 = function (name) { return console.log("Hello ".concat(name)); };
+greet1("Prasadh");
---
+++ hello/arrowfun.ts
@@ -0,0 +1,11 @@
+// Arrow functions
+
+// normal functoion
+function greet(name) {
+  console.log(`Hello ${name}`);
+}
+greet("Arun");
+
+// Arrow function
+const greet1 = (name) => console.log(`Hello ${name}`);
+greet1("Prasadh");
